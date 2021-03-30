507bc4bd0df8e8a0bbd18f5a219cdebc623755d1
Peik Etzell <peik.etzell@gmail.com>
2021-03-30 14:49:07 +0300

    Quite a bit modified, but now able to draw rectangles and lines onto canvas

--- src/main/scala/draw/Element.scala
+++ src/main/scala/draw/Element.scala
@@ -1,23 +1,63 @@
 package draw
 import scala.swing._
+import java.awt.Color._
 
-abstract class Element {
-    private var size: (Int, Int) = ???
-    private var position: (Int, Int) = ???
-    
+abstract class Element(var start: Point) {
+
     def rotate() = ???
-    def translate() = ???
-    def scale() = ???
+    def translate(xdir: Int, ydir: Int) = {
+        this.start.translate(xdir, ydir)
+    }
+    def scale(factor: Double) = {
+        ???
+    }
+
+    def draw(g: Graphics2D)
+    def getBounds() = ???
 }
 
-abstract class Shape extends Element
+abstract class Shape(start: Point, var color: Color = Preferences.color) extends Element(start) {
+    var end: Point = start
+}
 
-class Oval extends Shape
-class Rectangle extends Shape
-class Line extends Shape
+class Oval(start: Point) extends Shape(start) {
+    def draw(g: Graphics2D) = {
+        
+    }
+}
 
-class TextBox extends Element
 
-class Grouping extends Element {
+class Rectangle(start: Point) extends Shape(start) {
+    private def dx = (start.x - end.x).abs
+    private def dy = (start.y - end.y).abs
+    
+    def draw(g: Graphics2D): Unit = {
+        import math._
+        val x = min(start.x, end.x)
+        val y = min(start.y, end.y)
+        
+        g.setColor(color)
+        g.fillRect(x, y, dx, dy)
+    }
+}
+
+class Line(start: Point) extends Shape(start) {
+    def draw(g: Graphics2D) = {
+        g.setColor(color)
+        g.drawLine(start.x, start.y, end.x, end.y)
+    }
+}
+
+class TextBox(start: Point) extends Element(start) {
+    def draw(g: Graphics2D) = ???
+}
+
+class Grouping(start: Point) extends Element(start) {
     private val elements = ???
+
+    def draw(g: Graphics2D) = {
+        
+    }
 }
+
+class Freehand(start: Point)
