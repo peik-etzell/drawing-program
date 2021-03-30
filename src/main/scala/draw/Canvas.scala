2e2303fd43af56c520e116b22d394ca2752a2a40
Peik Etzell <peik.etzell@gmail.com>
2021-03-30 14:45:59 +0300

    Added Undo-Redo capability and simplified everything else

--- src/main/scala/draw/Canvas.scala
+++ src/main/scala/draw/Canvas.scala
@@ -1,17 +1,38 @@
 package draw
 import scala.swing._
 import java.awt.Color._
+import scala.collection.mutable.Buffer
+import scala.swing.event.MouseDragged
 
-object Canvas extends Panel {
-    override def paintComponent(g: Graphics2D): Unit = {
-        preferredSize = new Dimension(600, 600)
-        
-        g.setColor(YELLOW)
-        g.fillRect(10, 10, 20, 20)
-        repaint()
-        def update() = ???
+class Canvas extends Panel {
+    private val elements: Buffer[Element] = Buffer()
+    private val undos: Buffer[Element] = Buffer()
+
+    def addElement(element: Element) = {
+        elements += element
+    }
+    
+    def undo() = {
+        if (elements.nonEmpty) {
+            val last = elements.last
+            undos += last
+            elements -= last
+        }
+    }
+    def redo() = {
+        if (undos.nonEmpty) {
+            val elem = undos.last
+            elements += elem
+            undos -= elem
+        }
     }
 
-
-
+    def clear() = {
+        elements.clear()
+        repaint()
+    }
+    
+    override def paintComponent(g: Graphics2D): Unit = {
+        elements.foreach(_.draw(g))
+    }
 }
