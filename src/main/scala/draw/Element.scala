aad86dc8c4cf417c8e3eb1423864d4be7f300d6d
Peik Etzell <peik.etzell@gmail.com>
2021-04-08 09:00:43 +0300

    Added rotation, scaling, translation functionality; Moved draw() heavy lifting into Element; Refactored Operations to use Mouse.drag

--- src/main/scala/draw/Element.scala
+++ src/main/scala/draw/Element.scala
@@ -1,81 +1,201 @@
 package draw
 import scala.swing._
 import java.awt.Color._
+import scala.collection.mutable.Buffer
+import java.awt.BasicStroke
 
-abstract class Element(var start: Point) {
-    def center: Point = ???
-    def rotate() = ???
-    def translate(xdir: Int, ydir: Int) = {
-        this.start.translate(xdir, ydir)
-    }
-    def scale(factor: Double) = {
-        ???
-    }
+trait Transformation
+case class Rotation(var theta: Double = 0.0) extends Transformation
+case class Scaling(var sx: Double = 1.0, var sy: Double = 1.0) extends Transformation
 
-    def draw(g: Graphics2D)
-    def hit(point: Point): Boolean = ???
-}
-
-abstract class Shape(start: Point) extends Element(start) {
+trait Element {
     var color: Color = Mode.color
-    var end: Point = start
-    var fill = Mode.fill
-}
+    var strokeSize = Mode.stroke
+    val transformations: Buffer[Transformation] = Buffer()
+    def points: Seq[Point]
+    def center: Point 
+    
+    def move(dx: Int, dy: Int) = this.points.foreach(_.translate(dx, dy))
 
-class Oval(start: Point) extends Shape(start) {
-    override def center = start
-    def radius: Int = start.distance(end).floor.toInt
+    def render(g: Graphics2D)
 
     def draw(g: Graphics2D) = {
         g.setColor(color)
+        g.setStroke(new BasicStroke(strokeSize))
+       
+        g.translate(center.x, center.y)
+        for (t <- transformations.reverse) {
+            t match {
+                case Rotation(theta) => {
+                    g.rotate(theta)
+                }
+                case Scaling(sx, sy) => {
+                    g.scale(sx, sy)
+                }
+            }
+        }
+        g.translate(-center.x, -center.y)
+
+        this.render(g)
+        
+        g.translate(center.x, center.y)
+        for (t <- transformations) {
+            t match {
+                case Rotation(theta) => {
+                    g.rotate(-theta)
+                }
+                case Scaling(sx, sy) => {
+                    g.scale(1/sx, 1/sy)
+                }
+            }
+        }
+        g.translate(-center.x, -center.y)
+    }
+
+    def hit(point: Point): Boolean = {
+        import math._
+        
+        var x: Double = point.x - center.x
+        var y: Double = point.y - center.y
+        
+        for (t <- transformations.reverse) {
+            t match {
+                case Rotation(theta) => {
+                    val tempx = x * cos(theta) + y * sin(theta)
+                    val tempy = y * cos(theta) + x * sin(-theta)
+                    x = tempx
+                    y = tempy
+                }
+                case Scaling(sx, sy) => {
+                    x /= sx
+                    y /= sy
+                }
+            }
+        }
+        val xInt = x.round.toInt + center.x
+        val yInt = y.round.toInt + center.y
+        inside(new Point(xInt, yInt))
+    }
+    def inside(point: Point): Boolean
+}
+
+abstract class Shape(var start: Point) extends Element {
+    var end: Point = start
+    var fill = Mode.fill
+    def setPoint(point: Point) = {
+        end = point
+    }
+
+    def points: Seq[Point] = Seq(start, end)
+}
+
+class Oval(start: Point) extends Shape(start) {
+    def center = start
+    
+    def r: Int = start.distance(end).floor.toInt
+    def inside(point: Point): Boolean = center.distance(point) <= r
+    def render(g: Graphics2D) = {
         if (fill) {
-            g.fillOval(start.x - radius / 2, start.y - radius / 2, radius, radius)
+            g.fillOval(start.x - r, start.y - r, 2 * r, 2 * r)
         } else {
-            g.drawOval(start.x - radius / 2, start.y - radius / 2, radius, radius)
+            g.drawOval(start.x - r, start.y - r, 2 * r, 2 * r)
         }
     }
 }
 
-
 class Rectangle(start: Point) extends Shape(start) {
-    private def dx = (start.x - end.x).abs
-    private def dy = (start.y - end.y).abs
+    import math._
+
+    private def width = (start.x - end.x).abs
+    private def height = (start.y - end.y).abs
     
-    def draw(g: Graphics2D): Unit = {
-        import math._
-        val x = min(start.x, end.x)
-        val y = min(start.y, end.y)
-        
-        g.setColor(color)
+    private def topx = min(start.x, end.x)
+    private def topy = min(start.y, end.y)
+
+    def center = {
+        new Point((start.x + end.x) / 2, (start.y + end.y) / 2)
+    }
+    
+    def inside(point: Point): Boolean = {
+        point.x >= topx && point.y >= topy && point.x <= topx + width && point.y <= topy + height
+    }
+    
+    def render(g: Graphics2D) = {
         if (fill) {
-            g.fillRect(x, y, dx, dy)
+            g.fillRect(topx, topy, width, height)
         } else {
-            g.drawRect(x, y, dx, dy)
+            g.drawRect(topx, topy, width, height)
         }   
     }
 }
 
 class Line(start: Point) extends Shape(start) {
-    override def center = {
-        new Point((start.x + end.x) / 2, (start.y + end.y) / 2)
-    }
-    
-    def draw(g: Graphics2D) = {
-        g.setColor(color)
-        g.drawLine(start.x, start.y, end.x, end.y)
-    }
+    def center = new Point((start.x + end.x) / 2, (start.y + end.y) / 2)
+    def render(g: Graphics2D) = g.drawLine(start.x, start.y, end.x, end.y)
+    def inside(point: Point): Boolean = ???
 }
 
-class TextBox(start: Point) extends Element(start) {
-    def draw(g: Graphics2D) = ???
+class TextBox(start: Point) extends Element {
+    def center: Point = ???
+    var points: Seq[Point] = Seq(start)
+    def render(g: Graphics2D) = {}
+    def inside(point: Point): Boolean = ???
 }
 
-class Grouping(start: Point) extends Element(start) {
+class Grouping(start: Point) extends Element {    
+    def center: Point = ???
     private val elements = ???
+    var points: Seq[Point] = ???
+    def inside(point: Point): Boolean = ???
 
-    def draw(g: Graphics2D) = {
-        
+    def render(g: Graphics2D) = {}
+}
+
+class Freehand(start: Point) extends Shape(start) {
+    private val line: Buffer[Point] = Buffer(start)
+    def center = new Point((line.head.x + line.last.x) / 2, (line.head.y + line.last.y) / 2)
+    
+    override def setPoint(point: Point) = {
+        line += point
+    }
+
+    def inside(point: Point): Boolean = {
+        line.exists(p => p.distance(point) <= 10)
+    }
+
+    override def points: Seq[Point] = line.toSeq
+
+    def render(g: Graphics2D) = {
+        val pointPairs = {
+            points.dropRight(1) zip points.tail
+        }
+        for (pair <- pointPairs) {
+            val start = pair._1
+            val end = pair._2
+            g.drawLine(start.x, start.y, end.x, end.y)
+        }
     }
 }
 
-class Freehand(start: Point)
+object Debug extends Element {
+    def center: Point = new Point(0, 0)
+    def points: Seq[Point] = ???
+    def inside(point: Point): Boolean = false
+    
+    def printout = f"""
+        color: ${Mode.color}
+        operation: ${Mode.operation}
+        fill: ${Mode.fill}
+        selected: ${Mode.selected}
+        """  
+    
+    def render(g: Graphics2D) = {
+        var height = 0
+        val lineSpacing = 15
+
+        for (line <- printout.linesIterator) {
+            height += lineSpacing
+            g.drawString(line, 10, height)
+        }
+    }
+}
--- src/main/scala/draw/Operation.scala
+++ src/main/scala/draw/Operation.scala
@@ -1,75 +1,178 @@
 package draw
 
 import java.awt.Point
-import java.awt.Color
+import java.awt.Color._
+import scala.util.Random
 
 object Mode {
-    var color: Color = Color.black
-    var operation: Operation = MakeRect
+    var operation: Operation = new MakeRect
+    var color = black
     var fill = false
-    var backgroundColor = Color.white
+    var stroke = 5
+    var backgroundColor = white
     var selected: Option[Element] = None
 }
 
 trait Operation {
-    def click(point: Point)
+    def press(point: Point)
     def move(point: Point)
+    def drag(point: Point)
+    def release()
+    def undo()
+    def redo()
 }
 
 object Select extends Operation {
-    def click(point: Point): Unit = {
+    def press(point: Point): Unit = {
         Mode.selected = GUI.canvas.elements.reverse.filter(_.hit(point)).headOption
     }
-    def move(point: Point): Unit = {}
+    def move(point: Point) = {}
+    def drag(point: Point): Unit = {}
+    def release(): Unit = {}
+    def undo() = {}
+    def redo() = {}
 }
 
-abstract class Maker extends Operation {
-    private var clicked = false
-    private var shape: Shape = null
-    
+trait Maker extends Operation {
+    var element: Option[Shape] = None
+
     def makeShape(point: Point): Shape
+    def reset(): Unit 
+    def press(point: Point) {}
+    def move(point: Point) {}
 
-    def click(point: Point) {
-        if (!clicked) {
-            shape = makeShape(point)
-            GUI.canvas.addElement(shape)
-            clicked = true 
+    def drag(point: Point): Unit = {
+        if (element.isEmpty) {
+            element = Some(makeShape(point))
+            GUI.canvas.addElement(element.get)
+            GUI.canvas.pushHistory(this)
         } else {
-            clicked = false
-        }
-    }
-
-    def move(point: Point) {
-        if (clicked) {
-            shape.end = point
+            element.foreach(_.setPoint(point))
             GUI.canvas.repaint()
         }
     }
+    def release(): Unit = reset()
+    def undo() = GUI.canvas.removeElement(element.get)
+    def redo() = GUI.canvas.addElement(element.get)
 }
 
-object MakeRect extends Maker {
+class MakeRect extends Maker {
     def makeShape(point: Point): Shape = new Rectangle(point)
+    def reset(): Unit = {
+        Mode.operation = new MakeRect
+    }
 }
-object MakeOval extends Maker {
+class MakeOval extends Maker {
     def makeShape(point: Point): Shape = new Oval(point)
+    def reset(): Unit = {
+        Mode.operation = new MakeOval
+    }
 }
-object MakeLine extends Maker {
+class MakeLine extends Maker {
     def makeShape(point: Point): Shape = new Line(point)
+    def reset(): Unit = {
+        Mode.operation = new MakeLine
+    }
+}
+class MakeFreehand extends Maker {
+    def makeShape(point: Point): Shape = new Freehand(point)
+    def reset(): Unit = {
+        Mode.operation = new MakeLine
+    }
 }
 
+trait Transform extends Operation {
+    val transformation: Transformation
+    var element: Option[Element] = None
+    var start: Option[Point] = None
+    
+    def press(point: Point): Unit = {
+        Select.press(point)
+    }
 
+    def release(): Unit
+    def drag(point: Point): Unit
+    def move(point: Point): Unit = {}
+    def undo(): Unit = element.foreach(_.transformations -= transformation)
+    def redo(): Unit = element.foreach(_.transformations += transformation)
 
-object Rotate extends Operation {
-    def click(point: Point): Unit = ???
-    def move(point: Point): Unit = ???
+}
+class Rotate extends Transform {
+    val transformation: Rotation = new Rotation
+    
+    def release(): Unit = {
+        Mode.operation = new Rotate
+    }
+    def drag(point: Point): Unit = {
+        if (Mode.selected.isDefined) {
+            if (start.isEmpty) {
+                element = Mode.selected
+                start = Some(point)
+                element.foreach(_.transformations += transformation)
+                GUI.canvas.pushHistory(this)
+            } else {
+                transformation.theta = 0.01 * (start.get.x - point.x)
+                GUI.canvas.repaint()
+            }
+        }
+    }
 }
 
-object Translate extends Operation {
-    def click(point: Point): Unit = ???
-    def move(point: Point): Unit = ???
+class Translate extends Operation {
+    var deltax = 0
+    var deltay = 0
+    var element: Option[Element] = None
+    var start: Option[Point] = None
+    
+    def press(point: Point): Unit = {
+        Select.press(point)
+    }
+
+    def release(): Unit = {
+        Mode.operation = new Translate
+    }
+    def drag(point: Point): Unit = {
+        if (Mode.selected.isDefined) {
+            if (start.isEmpty) {
+                element = Mode.selected
+                start = Some(point)
+                
+                GUI.canvas.pushHistory(this)
+            } else {
+                val dx = point.x - start.get.x
+                val dy = point.y - start.get.y
+                start = Some(point)
+                deltax += dx
+                deltay += dy
+                element.foreach(_.move(dx, dy))
+                GUI.canvas.repaint()
+            }
+        }
+    }
+    def move(point: Point): Unit = {}
+    def undo(): Unit = element.foreach(_.move(-deltax, -deltay))
+    def redo(): Unit = element.foreach(_.move(deltax, deltay))
+
 }
 
-object Scale extends Operation {
-    def click(point: Point): Unit = ???
-    def move(point: Point): Unit = ???
+class Scale extends Transform {
+    val transformation: Scaling = new Scaling
+    
+    def release(): Unit = {
+        Mode.operation = new Scale
+    }
+    def drag(point: Point): Unit = {
+        if (Mode.selected.isDefined) {
+            if (start.isEmpty) {
+                element = Mode.selected
+                start = Some(point)
+                element.foreach(_.transformations += transformation)
+                GUI.canvas.pushHistory(this)
+            } else {
+                transformation.sx = 0.005 * (start.get.x - point.x) + 1.0
+                transformation.sy = 0.005 * (start.get.y - point.y) + 1.0
+                GUI.canvas.repaint()
+            }
+        }
+    }
 }
