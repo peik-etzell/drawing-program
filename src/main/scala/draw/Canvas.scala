d63923e6fa21da01b728cfe2bbf7d6ec5fe12d5c
Peik Etzell <peik.etzell@gmail.com>
2021-04-15 07:34:13 +0300

    Added creation variable for future loading

--- src/main/scala/draw/Canvas.scala
+++ src/main/scala/draw/Canvas.scala
@@ -2,26 +2,23 @@ package draw
 import scala.swing._
 import java.awt.Color._
 import scala.collection.mutable.Buffer
-import scala.swing.event.MouseMoved
-import scala.swing.event.MouseClicked
 import scala.collection.mutable.Stack
 import scala.swing.event.MouseDragged
 import scala.swing.event.MouseReleased
 import scala.swing.event.MousePressed
 
-class Canvas extends Panel {
-    val elements: Buffer[Element] = Buffer()
+class Canvas(val elements: Buffer[Element] = Buffer()) extends Panel {
+    
     private val history: Stack[Operation] = Stack()
     private val undone: Stack[Operation] = Stack()
+
+    preferredSize = new Dimension(1280, 960)
 
     listenTo(mouse.clicks)
     listenTo(mouse.moves)
 
 
     reactions += {
-        case MouseMoved(_, point, _) => {
-            Mode.operation.move(point)
-        }
         case MousePressed(_, point, _, _, _) => {
             Mode.operation.press(point)
         }
